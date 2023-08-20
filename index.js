0ac825fdc246e9f440b6f66ae415ca765fb5fc78
rajesh kannan <itisrajeshkannan@gmail.com>
2023-08-21 00:53:21 +0530

    third commit

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 var firebase = require('firebase/compat/app');
+
+  
+
 require('firebase/compat/database')
-const firebaseConfig = {
-    // apiKey: "AIzaSyCK9BP8CrsdBd3cX65gnaV1dzTXpr4YdMU",
-    // authDomain: "test-database-60e38.firebaseapp.com",
-    databaseURL: "https://test-database-60e38-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "test-database-60e38",
-    // storageBucket: "test-database-60e38.appspot.com",
-    // messagingSenderId: "762127810386",
-    // appId: "1:762127810386:web:78c4a71d1bdbf960d0448b",
-    // measurementId: "G-2T9VV5N1LX"
-  };
-  firebase.initializeApp(firebaseConfig)
-  let data;
-firebase.database().ref("user").on("value", snapshot =>{
-  data = snapshot.val();
-})
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8081;
 
 app.get('/',(req, res) => {
+    var table = req.query.table
+    var databaseURL = req.query.databaseURL
+    var projectId = req.query.projectId
+    const firebaseConfig = {
+        databaseURL: databaseURL || "https://test-database-60e38-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: projectId || "test-database-60e38",
+      };
+      firebase.initializeApp(firebaseConfig)
+      let data;
+    firebase.database().ref(table || "user").on("value", snapshot =>{
+      data = snapshot.val();
+    })
     res.json(data)
 })
 
